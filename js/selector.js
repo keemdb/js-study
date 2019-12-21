0d5dd58ece784a7c590aeaa5c2bdd4f5cab62130
unknown <keemdb@gmail.com>
2019-12-21 18:06:59 +0900

    DDD

---
+++ js/selector.js
@@ -0,0 +1,9 @@
+var log = console.log;
+var lis = document.getElementsByClassName('act');
+
+for (var i=0; i<lis.length; i++){
+	lis[i].style.color = 'red';
+}
+
+
+log(lis);
---
+++ js/window-open.js
@@ -0,0 +1,4 @@
+var n = 1;
+var end = $('.banner').length - 1;
+var interval = setInterval (ani, 2000);
+
